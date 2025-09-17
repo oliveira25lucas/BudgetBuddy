7752557c9afb4069d3572c421dd8f2a2a2932456
oliveira25lucas <lucas1122oliveira@gmail.com>
2025-09-17 11:55:24 -0300

    feat: add spring

---
+++ src/main/java/com/oliveira25lucas/BudgetBuddy/BudgetBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira25lucas.BudgetBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBuddyApplication.class, args);
+	}
+
+}
